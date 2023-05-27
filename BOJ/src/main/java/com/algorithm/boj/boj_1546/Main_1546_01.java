a083b675401c7c2e810c70797ba6ecbed1666b9a
psjw <dlrudtn108@gmail.com>
2023-05-27 23:48:44 +0900

    second commit

--- BOJ/src/main/java/com/algorithm/boj/boj_1546/Main_1546_01.java
+++ BOJ/src/main/java/com/algorithm/boj/boj_1546/Main_1546_01.java
@@ -1,7 +1,23 @@
 package com.algorithm.boj.boj_1546;
 
+import java.io.*;
+import java.util.StringTokenizer;
 public class Main_1546_01 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+            double N = Double.parseDouble(br.readLine());  //최고점
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double max = 0F;
+            double sum = 0F;
+            for(int i = 0; i<N ;i++){
+                double temp = Double.parseDouble(st.nextToken());
+                sum += temp;
+                max = Math.max(temp, max);
+            }
+            double avg = ((sum*100)/ max) / N;
+            bw.write(avg + "\n");
+            bw.flush();
+        }
     }
 }
