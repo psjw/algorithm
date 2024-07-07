a3d60efb9eec396264a6df9d4a109bea99fb56af
psjw <dlrudtn108@gmail.com>
2024-07-07 18:27:07 +0900

    first commit

---
+++ LeetCode/src/_234_Palindrome_Linked_List/PalindromeLinkedList.java
@@ -0,0 +1,11 @@
+package _234_Palindrome_Linked_List;
+
+public class PalindromeLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+//    public boolean isPalindrome(ListNode head) {
+//
+//    }
+}
