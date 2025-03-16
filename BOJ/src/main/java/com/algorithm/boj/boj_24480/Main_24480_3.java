6c7a3426506e569e5245e7336bc83c3f3236a989
psjw <dlrudtn108@gmail.com>
2025-03-16 22:11:19 +0900

    firstcommit

--- BOJ/src/main/java/com/algorithm/boj/boj_24480/Main_24480_3.java
+++ BOJ/src/main/java/com/algorithm/boj/boj_24480/Main_24480_3.java
@@ -1,43 +1,46 @@
-package com.algorithm.boj.boj_24479;
+package com.algorithm.boj.boj_24480;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
-public class Main_24479_4 {
+public class Main_24480_3 {
     private static int N, M, R;
-    private static int[] visited;
-    private static ArrayList<Integer>[] graph;
+    private static ArrayList<Integer> graph[];
+    private static int visited[];
     private static int RESULT;
 
-
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));){
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             R = Integer.parseInt(st.nextToken());
 
-            graph = new ArrayList[N + 1];
-            visited = new int[N + 1];
+            graph = new ArrayList[N+1];
+            visited = new int[N+1];
 
-            IntStream.rangeClosed(0, N).forEach(x -> graph[x] = new ArrayList<>());
+            IntStream.rangeClosed(0, N).forEach(i -> {
+                graph[i] = new ArrayList<>();
+            });
 
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
+
                 graph[x].add(y);
                 graph[y].add(x);
             }
 
-            IntStream.rangeClosed(0, N).forEach(x -> Collections.sort(graph[x]));
+            Arrays.stream(graph).forEach(x -> Collections.sort(x,Collections.reverseOrder()));
 
             dfs(R);
+
             IntStream.rangeClosed(1, N).forEach(i -> {
                 try {
                     bw.write(String.valueOf(visited[i]));
@@ -48,7 +51,6 @@ public class Main_24479_4 {
             });
 
             bw.flush();
-
         }
     }
 
@@ -56,9 +58,9 @@ public class Main_24479_4 {
         RESULT++;
         visited[idx] = RESULT;
 
-        for (int j = 0; j < graph[idx].size(); j++) {
-            Integer nextIdx = graph[idx].get(j);
-            if (visited[nextIdx] == 0) {
+        for(int i =0 ; i < graph[idx].size(); i++) {
+            Integer nextIdx = graph[idx].get(i);
+            if(visited[nextIdx]==0) {
                 dfs(nextIdx);
             }
         }
