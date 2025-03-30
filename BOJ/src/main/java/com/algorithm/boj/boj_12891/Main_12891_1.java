9126c1c28e2324433263eb3859e9b1d61979ce9a
psjw <dlrudtn108@gmail.com>
2025-03-30 15:13:53 +0900

    first

--- BOJ/src/main/java/com/algorithm/boj/boj_12891/Main_12891_1.java
+++ BOJ/src/main/java/com/algorithm/boj/boj_12891/Main_12891_1.java
@@ -6,7 +6,8 @@ import java.util.StringTokenizer;
 public class Main_12891_1 {
     private static int S, P, answer;
     private static char[] arr;
-    private static int count[] = new int[4];
+    private static int checkArr[] = new int[4];
+    private static int myArr[] = new int[4];
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,25 +16,67 @@ public class Main_12891_1 {
             int S = Integer.parseInt(st.nextToken());
             int P = Integer.parseInt(st.nextToken());
             arr = new char[S];
-            String temp = br.readLine();
-            for (int i = 0; i < temp.length(); i++) {
-                arr[i] = temp.charAt(i) ;
-            }
-            for (int i = 0; i < count.length; i++) {
-                st = new StringTokenizer(br.readLine());
-                count[i] = Integer.parseInt(st.nextToken());
-            }
+            arr = br.readLine().toCharArray();
 
-            int start = 0;
-            int end = start;
-
-            for(int i = 0; i<S ;i++){
-
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < checkArr.length; i++) {
+                checkArr[i] = Integer.parseInt(st.nextToken());
             }
 
 
+            for (int i = 0; i < P; i++) {
+                addArr(arr[i]);
+            }
+            if(validCheckArr()){
+                answer++;
+            }
 
+            for (int i = P; i < S; i++) {
+                int j = i - P;
 
+                addArr(arr[i]);
+                removeArr(arr[j]);
+                if(validCheckArr()){
+                    answer++;
+                }
+
+            }
+            bw.write(String.valueOf(answer));
+            bw.flush();
+
+        }
+    }
+
+    private static boolean validCheckArr() {
+        for(int j =0; j< myArr.length;j++){
+            if(myArr[j] < checkArr[j]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void addArr(char ch) {
+        if (ch == 'A') {
+            myArr[0]++;
+        } else if (ch == 'C') {
+            myArr[1]++;
+        } else if (ch == 'G') {
+            myArr[2]++;
+        } else if (ch == 'T') {
+            myArr[3]++;
+        }
+    }
+
+    private static void removeArr(char ch) {
+        if (ch == 'A') {
+            myArr[0]--;
+        } else if (ch == 'C') {
+            myArr[1]--;
+        } else if (ch == 'G') {
+            myArr[2]--;
+        } else if (ch == 'T') {
+            myArr[3]--;
         }
     }
 }
