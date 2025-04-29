dcdaeda0da14bdbce0a2a5b8c6284c8be6220958
psjw <dlrudtn108@gmail.com>
2025-04-29 21:39:29 +0900

    array

---
+++ Programmers/src/main/java/pccp/ch02/MainArrayV0.java
@@ -0,0 +1,25 @@
+package pccp.ch02;
+
+import java.util.Arrays;
+
+public class MainArrayV0 {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        System.out.println(arr);
+        System.out.println(Arrays.toString(arr));
+        arr[0] = 1;
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr[0]);
+        arr[4] = 5;
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr.length);
+        arr[arr.length - 1] = 5;
+        System.out.println(Arrays.toString(arr));
+
+        String[] strs = new String[5];
+        System.out.println(Arrays.toString(strs));
+        boolean[] bools = new boolean[5];
+        System.out.println(Arrays.toString(bools));
+
+    }
+}
